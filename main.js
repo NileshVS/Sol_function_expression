3c2c10260a7cd60728b193915ecca2f3fb72fb64
NileshVS <50776400+NileshVS@users.noreply.github.com>
2019-05-22 23:24:36 +0530

    code commit

---
+++ main.js
@@ -0,0 +1,10 @@
+//function expression
+
+let funcAdd=function(value1,value2){
+    return value1+value2;
+}
+
+console.log(funcAdd(5,3));
+
+//Cannot be called before declaration unlike normal function declaration, as it is defined as a variable
+//and follows variable rules.
